cb1d064294bbb11dbfd36feffce81a58d56a003d
raimohan <raimohan9676@gmail.com>
2025-08-25 17:14:35 +0530

    Update builder.tsx

--- src/pages/builder.tsx
+++ src/pages/builder.tsx
@@ -22,7 +22,7 @@ export default function Builder() {
         variant: "destructive",
       });
       setTimeout(() => {
-        window.location.href = "/api/login";
+        setLocation("/auth");
       }, 500);
       return;
     }
